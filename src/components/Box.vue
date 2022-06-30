0caa7644343eb54d1842b926bed55bd81c97a77d
Alanfdsilva <alanfd.silva@gmail.com>
2022-06-30 20:07:05 -0300

    first commit

---
+++ src/components/Box.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box box has-text-weight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "Box-1",
+    data() {
+        return {
+            estilos: {
+                background: '#FAF0CA'
+            }
+        }
+    }
+})
+
+</script>
